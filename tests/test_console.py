fa12ffb1bff62447e4b4aa172096efb098361144
LucileSD <4316@holbertonschool.com>
2022-08-11 10:08:14 +0000

    console

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,4 +4,25 @@ Unit tests for tha HBNB console
 """
 import unittest
 import pycodestyle
+from console import HBNBCommand
+from models.base_model import BaseModel
+from datetime import datetime
+import console
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """ Test class HBNBCommand """
+    def test_class_style(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0)
+
+    def test_prompt(self):
+        """test the prompt format"""
+        self.assertEqual(HBNBCommand.prompt, "(hbnb) ")
+
+
+if __name__ == '__main__':
+    unittest.main()
 
