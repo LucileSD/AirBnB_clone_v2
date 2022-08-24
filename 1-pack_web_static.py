d48f297360aa2a9851e21390575276deb0ba405b
LucileSD <4316@holbertonschool.com>
2022-08-24 10:56:13 +0000

    tar

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    generates a .tgz archive from the contents of the web_static folder
+    of your AirBnB Clone repo
+"""
+from fabric.api import local
+import time
+
+
+def do_pack():
+    """
+    generates a .tgz archive from the contents of the web_static folder
+    of your AirBnB Clone repo
+    """
+    date = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local('mkdir -p versions')
+        local('tar -cvzf versions/web_static_{}.tgz web_static'.format(date))
+        return 'versions/web_static_{}.tgz'.format(date)
+    except Exception:
+        return None
