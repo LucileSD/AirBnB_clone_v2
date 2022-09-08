de6d503023e6856e44eb58d72ecf4e4c02a9636f
LucileSD <4316@holbertonschool.com>
2022-09-08 08:31:05 +0000

    hello

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,6 +6,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
     """ just say hello"""
@@ -14,4 +15,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
