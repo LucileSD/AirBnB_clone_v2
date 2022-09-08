15be8e4b596a0d092cd81ef822a4c312eca16f77
LucileSD <4316@holbertonschool.com>
2022-09-08 10:41:24 +0000

    2 route for the same method

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ just say hello"""
+    message = "Hello HBNB!"
+    return message
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ say hbnb"""
+    message = "HBNB"
+    return message
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def CIsFun(text):
+    """ take the value of a variable"""
+    newText = text.replace("_", " ")
+    message = "C " + newText
+    return message
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonIs(text="is cool"):
+    """ take the value of a variable"""
+    newText = text.replace("_", " ")
+    message = "Python " + newText
+    return message
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
