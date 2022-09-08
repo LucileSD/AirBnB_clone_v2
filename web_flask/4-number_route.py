7c5c6a564d5a088580515a73ccb85c495c95ad00
LucileSD <4316@holbertonschool.com>
2022-09-08 10:57:14 +0000

    only if is int

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ just say hello"""
+    message = "Hello HBNB!"
+    return message
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ say hbnb"""
+    message = "HBNB"
+    return message
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def CIsFun(text):
+    """ take the value of a variable"""
+    newText = text.replace("_", " ")
+    message = "C " + newText
+    return message
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def pythonIs(text="is cool"):
+    """ take the value of a variable"""
+    newText = text.replace("_", " ")
+    message = "Python " + newText
+    return message
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def isNumber(n):
+    """ display “n is a number” only if n is an integer"""
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
