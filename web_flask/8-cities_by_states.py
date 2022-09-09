dcda21b80f7c4f9f7c062add1b3c43aabf101b78
LucileSD <4316@holbertonschool.com>
2022-09-09 07:44:50 +0000

    inner loop for cities

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def removeSql(exception):
+    """  remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def listState():
+    """list states in html page"""
+    list = storage.all(State).values()
+    return render_template("7-states_list.html", list=list)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def listCity():
+    """list cities in a state"""
+    list = storage.all(State).values()
+    return render_template("8-cities_by_states.html", list=list)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
