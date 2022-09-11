6bb9872f74487f0b9d41693d08ab430c6b954bd3
LucileSD <4316@holbertonschool.com>
2022-09-11 14:05:40 +0000

    correction html

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -16,18 +16,10 @@ def removeSql(exception):
 
 
 @app.route("/states", strict_slashes=False)
-@app.route("/states_list", strict_slashes=False)
 def listState():
     """list states in html page"""
     list = storage.all(State).values()
     return render_template("7-states_list.html", list=list)
-
-
-@app.route("/cities_by_states", strict_slashes=False)
-def listCity():
-    """list cities in a state"""
-    list = storage.all(State).values()
-    return render_template("8-cities_by_states.html", list=list)
 
 
 @app.route("/states/<id>", strict_slashes=False)
@@ -38,7 +30,9 @@ def listStateId(id):
     for element in list:
         if (element.id == id):
             listState.append(element)
-    return render_template("9-states.html", list=listState)
+            return render_template("9-states.html", list=listState,
+                                   state=element, flag = 1)
+    return render_template("9-states.html")
 
 
 if __name__ == '__main__':
